37fbaceb48fad1fd140a6de2dbca4280b99222c3
Evan Clevenger <evanclevenger@gmail.com>
2024-12-08 20:55:39 -0600

    added 'card' and 'dashboard' as components

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        white: "white",
+        none: "none",
+      },
+      borderWidth: {
+        1: "1px",
+      },
+      fontFamily: {
+        robotoMono: ["Roboto Mono", "serif"],
+      },
+      gridTemplateRows: {
+        //creates 7 OR 8 equally sized rows
+        7: "repeat(7, minmax(0, 1fr))",
+        8: "repeat(8, minmax(0, 1fr))",
+      },
+    },
+  },
+  plugins: [],
+};
